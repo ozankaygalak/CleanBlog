0474f6ff336554cd02307add4b54459aa6d15f93
[Ozan Kaygalak] <[ozankaygalak@gmail.com]>
2023-07-16 15:44:08 +0300

    add get method

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const ejs = require('ejs');
 const path = require('path');
 const app = express();
 app.use(express.static('public'));
-app.set('view engine','ejs');
-app.get('/', (req,res)=>{
-    res.render('index')
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+  res.render('index');
 });
-app.get('/about',(req,res)=>{
-    res.render('about')
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
-app.get('/contact',(req,res)=>{
-    res.render('contact')
+app.get('/contact', (req, res) => {
+  res.render('contact');
 });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`server ${port} çalışıyor`)
+  console.log(`server ${port} çalışıyor`);
 });
