8083cdc1455886c80886d28beddfd1d74277628d
[Ozan Kaygalak] <[ozankaygalak@gmail.com]>
2023-07-18 16:20:25 +0300

    create models

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
 const app = express();
+const Photos = require('./models/Photos')
+const mongoose = require('mongoose');
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-  res.render('index');
+mongoose.connect('mongodb://localhost:27017/clean-blog',{
+  useNewUrlParser:true,
+  useUnifiedTopology: true
+});
+
+app.get('/', async (req, res) => {
+  const blogs = Photos.find({});
+  await res.render('index',{blogs});
 });
 app.get('/about', (req, res) => {
   res.render('about');
---
+++ models/Photos.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const BlogSchema = new Schema ({
+    description: String,
+    message : String,
+    photo : String,
+    dateCreated : {
+        type : Date,
+        default : Date.now
+    }
+});
+
+const Photos = mongoose.model('Photos', BlogSchema);
+
+module.exports= Photos;
